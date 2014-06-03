68ebea64ee8f956a2f762af7610e2d01ebbf8321
Alejandro Pedraza <alejandro.pedraza@gmail.com>
2014-06-03 16:33:01 -0500

    lang - replaced old comments marker /*T*/ with new one lang()

--- PluginEprocessingnetwork.php
+++ PluginEprocessingnetwork.php
@@ -18,79 +18,79 @@ class PluginEprocessingnetwork extends GatewayPlugin
               value       - default value
         */
         $variables = array (
-                   /*T*/"Plugin Name"/*/T*/ => array (
+                   lang("Plugin Name") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"How CE sees this plugin (not to be confused with the Signup Name)"/*/T*/,
-                                        "value"         =>/*T*/"eProcessingNetwork"/*/T*/
+                                        "description"   =>lang("How CE sees this plugin (not to be confused with the Signup Name)"),
+                                        "value"         =>lang("eProcessingNetwork")
                                        ),
-                   /*T*/"eProcessingNetwork Username"/*/T*/ => array (
+                   lang("eProcessingNetwork Username") => array (
                                         "type"          =>"text",
-                                        "description"   =>/*T*/"Please enter your eProcessingNetwork Username Here."/*/T*/,
+                                        "description"   =>lang("Please enter your eProcessingNetwork Username Here."),
                                         "value"         =>""
                                        ),
-                   /*T*/"eProcessingNetwork Transaction Key"/*/T*/ => array (
+                   lang("eProcessingNetwork Transaction Key") => array (
                                         "type"          =>"password",
-                                        "description"   =>/*T*/"Please enter your eProcessingNetwork Transaction Key Here."/*/T*/,
+                                        "description"   =>lang("Please enter your eProcessingNetwork Transaction Key Here."),
                                         "value"         =>""
                                        ),
-                   /*T*/"Demo Mode"/*/T*/ => array (
+                   lang("Demo Mode") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES if you want to set this plugin in Demo mode for testing purposes."/*/T*/,
+                                        "description"   =>lang("Select YES if you want to set this plugin in Demo mode for testing purposes."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Accept CC Number"/*/T*/ => array (
+                   lang("Accept CC Number") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"Selecting YES allows the entering of CC numbers when using this plugin type. No will prevent entering of cc information"/*/T*/,
+                                        "description"   =>lang("Selecting YES allows the entering of CC numbers when using this plugin type. No will prevent entering of cc information"),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Visa"/*/T*/ => array (
+                   lang("Visa") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow Visa card acceptance with this plugin.  No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow Visa card acceptance with this plugin.  No will prevent this card type."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"MasterCard"/*/T*/ => array (
+                   lang("MasterCard") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow MasterCard acceptance with this plugin. No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow MasterCard acceptance with this plugin. No will prevent this card type."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"AmericanExpress"/*/T*/ => array (
+                   lang("AmericanExpress") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow American Express card acceptance with this plugin. No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow American Express card acceptance with this plugin. No will prevent this card type."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Discover"/*/T*/ => array (
+                   lang("Discover") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES to allow Discover card acceptance with this plugin. No will prevent this card type."/*/T*/,
+                                        "description"   =>lang("Select YES to allow Discover card acceptance with this plugin. No will prevent this card type."),
                                         "value"         =>"0"
                                        ),
-                   /*T*/"Invoice After Signup"/*/T*/ => array (
+                   lang("Invoice After Signup") => array (
                                         "type"          =>"yesno",
-                                        "description"   =>/*T*/"Select YES if you want an invoice sent to the customer after signup is complete."/*/T*/,
+                                        "description"   =>lang("Select YES if you want an invoice sent to the customer after signup is complete."),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"Signup Name"/*/T*/ => array (
+                   lang("Signup Name") => array (
                                         "type"          =>"text",
-                                        "description"   =>/*T*/"Select the name to display in the signup process for this payment type. Example: eCheck or Credit Card."/*/T*/,
+                                        "description"   =>lang("Select the name to display in the signup process for this payment type. Example: eCheck or Credit Card."),
                                         "value"         =>"Credit Card"
                                        ),
-                   /*T*/"Dummy Plugin"/*/T*/ => array (
+                   lang("Dummy Plugin") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"1 = Only used to specify a billing type for a customer. 0 = full fledged plugin requiring complete functions"/*/T*/,
+                                        "description"   =>lang("1 = Only used to specify a billing type for a customer. 0 = full fledged plugin requiring complete functions"),
                                         "value"         =>"0"
                                        ),
-                   /*T*/"Auto Payment"/*/T*/ => array (
+                   lang("Auto Payment") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"No description"/*/T*/,
+                                        "description"   =>lang("No description"),
                                         "value"         =>"1"
                                        ),
-                   /*T*/"30 Day Billing"/*/T*/ => array (
+                   lang("30 Day Billing") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"Select YES if you want ClientExec to treat monthly billing by 30 day intervals.  If you select NO then the same day will be used to determine intervals."/*/T*/,
+                                        "description"   =>lang("Select YES if you want ClientExec to treat monthly billing by 30 day intervals.  If you select NO then the same day will be used to determine intervals."),
                                         "value"         =>"0"
                                        ),
-                   /*T*/"Check CVV2"/*/T*/ => array (
+                   lang("Check CVV2") => array (
                                         "type"          =>"hidden",
-                                        "description"   =>/*T*/"Select YES if you want to accept CVV2 for this plugin."/*/T*/,
+                                        "description"   =>lang("Select YES if you want to accept CVV2 for this plugin."),
                                         "value"         =>"1"
                                        )
         );
